426bb553cb83c3d8b69521767386d36c4da8fc3d
pAc <pacthecool1@gmail.com>
2023-12-30 06:28:17 +0100

    nombre de joueurs co

--- public/js/client.js
+++ public/js/client.js
@@ -8,8 +8,11 @@ socket.on('updatePlayers', (data) => {
     // Mettez à jour l'état du jeu ici en fonction des données reçues
 });
 
-// Vous pouvez également envoyer des informations au serveur
-function sendPlayerMovement(movement) {
-    socket.emit('playerMovement', movement);
-}
+socket.on('userCount', (count) => {
+    // Mettre à jour l'affichage du nombre de joueurs connectés
+    document.getElementById('playerCount').innerText = count + ' joueurs connectés';
+});
+
+
+
 
--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+
 // Créer une application Express
 const app = express();
+
 // Servir les fichiers statiques du dossier spécifié
-app.use(express.static('public')); 
+app.use(express.static('public'));
+
 // Créer un serveur HTTP à partir de l'application Express
 const server = http.createServer(app);
+
 // Attacher Socket.IO au serveur
 const io = socketIo(server);
 
+let connectedUsers = 0;
+
 io.on('connection', (socket) => {
-    console.log('Un joueur sest connecté');
+    connectedUsers++;
+    console.log('Un joueur est connecté');
+    io.emit('userCount', connectedUsers); // Envoyer le nombre de joueurs connectés à tous les clients
 
-    socket.on('playerMovement', (data) => {
-        // Envoyer les données de mouvement à tous les clients sauf à celui qui les a envoyées
-        socket.broadcast.emit('updatePlayers', data);
-    });
-
+    
     socket.on('disconnect', () => {
-        console.log('Un joueur sest déconnecté');
+        connectedUsers--;
+        console.log('Un joueur est déconnecté');
+        io.emit('userCount', connectedUsers); // Mettre à jour le compteur pour tous les clients
     });
 });
 
 server.listen(3000, () => {
     console.log('Serveur démarré sur http://localhost:3000');
 });
+
 
 
